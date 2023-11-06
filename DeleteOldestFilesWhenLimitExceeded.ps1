328deb5ba4167babd186f242aff4e6eb01b6b919
martinbednar <martin.bedn20@gmail.com>
2023-11-06 12:20:45 +0100

    Move old pictures to archive (do not delete them).

--- DeleteOldestFilesWhenLimitExceeded.ps1
+++ DeleteOldestFilesWhenLimitExceeded.ps1
@@ -1,4 +1,4 @@
-$NumberToSave = 200
+$NumberToSave = 1000
 
 
 $items = Get-ChildItem "C:\FTP\ST100"  | Sort-Object Name -Descending
@@ -9,6 +9,19 @@ if ($NumberToSave -lt $items.Count)
 
 
 $items = Get-ChildItem "C:\FTP\ST200"  | Sort-Object Name -Descending
+if ($NumberToSave -lt $items.Count)
+{
+    $items[$NumberToSave..($items.Count-1)] | Remove-Item
+}
+
+$items = Get-ChildItem "C:\FTP\ST100_old"  | Sort-Object Name -Descending
+if ($NumberToSave -lt $items.Count)
+{
+    $items[$NumberToSave..($items.Count-1)] | Remove-Item
+}
+
+
+$items = Get-ChildItem "C:\FTP\ST200_old"  | Sort-Object Name -Descending
 if ($NumberToSave -lt $items.Count)
 {
     $items[$NumberToSave..($items.Count-1)] | Remove-Item
---
+++ MoveEverythingInFolders.ps1
@@ -0,0 +1,2 @@
+Get-ChildItem -Path C:\FTP\ST100\ -Recurse -File | Move-Item -Destination C:\FTP\ST100_old\
+Get-ChildItem -Path C:\FTP\ST200\ -Recurse -File | Move-Item -Destination C:\FTP\ST200_old\
